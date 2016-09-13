b68027d1801e6a8d2eede3dec7e91b4c2da3c63e
Naoki Sugimoto <assoonasp_t@yahoo.co.jp>
2016-09-13 15:26:46 +0900

    initial commit

---
+++ printer.rb
@@ -0,0 +1,23 @@
+changed = [3, 7, 12, 54, 13, 8, 9, 14, 12, 2, 4, 1, -7, -5]
+changed.sort!.uniq!.delete_if { |e| e <= 0 }
+
+result = []
+changed.each_index do |i|
+  break if i == changed.length - 1
+  result.push(changed[0]) if i == 0
+  if changed[i + 1] - changed[i] == 1
+    result.push('-')
+  elsif
+    result.push(changed[i], ',', changed[i + 1])
+  end
+end
+
+code = []
+result.each do |member|
+  if member != '-'
+    code.push(member)
+  elsif code.last != '-'
+    code.push('-')
+  end
+end
+p code.join
