e9bd56ee7838b93bdf9a61c0782ce58aa5d1a887
Naoki Sugimoto <assoonasp_t@yahoo.co.jp>
2016-09-19 16:01:27 +0900

    delete & uniq before sort

--- printer.rb
+++ printer.rb
@@ -1,12 +1,12 @@
 changed = [3, 7, 12, 54, 13, 8, 9, 14, 12, 2, 4, 1, -7, -5] # input array
-changed.sort!.uniq!.delete_if { |e| e <= 0 }
+changed.delete_if { |e| e <= 0 }.uniq!.sort!
 
 result = []
 changed.each_index do |i|
   break if i == changed.length - 1
   result.push(changed[0]) if i == 0
   if changed[i + 1] - changed[i] == 1
-    result.push('-')
+    result.push('-')\
   elsif
     result.push(changed[i], ',', changed[i + 1])
   end
@@ -21,4 +21,4 @@ result.each do |member|
   end
 end
 
-puts  code.join
+puts code.join
